98a97f6ceb5e5eb7264e6ee6cac5a47fdbbd01aa
SergeyNVKZ1991 <sergeynekrasov29121991@gmail.com>
2024-05-01 22:48:06 +0700

    add images

--- hello/firstapp/urls.py
+++ hello/firstapp/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.views.generic import TemplateView
+
 from .import views
 
 urlpatterns = [
     path('', views.index),
-
+    path('about/', TemplateView.as_view(template_name="first_app/about.html")),
+    path('contact/', TemplateView.as_view(template_name="first_app/contact.html",
+                                          extra_context={"work": "Поменял значение work"})),
 ]
---
+++ hello/hello/view.py
@@ -0,0 +1,6 @@
+import datetime
+from django.shortcuts import render
+from django.template.response import TemplateResponse
+
+def index(request):
+    return render(request, "first_app/index.html")
